e46903ccde067f301d301b4d1ef385c2ca419651
PriyaVP-crypto <56911834+PriyaVP-crypto@users.noreply.github.com>
2019-11-12 17:30:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,7 @@ class revRequ(tornado.web.RequestHandler):
         # 100000001001 is the only working answer
         headers = {'Content-Type': 'application/json'}
         end_url= base_url+str(self.get_body_argument("accnt"))+"&tranid="+str(self.get_body_argument("trans"))+"&revamt="+str(self.get_body_argument("debit_amt"))
+        #end_url= base_url+str(self.get_body_argument("accnt"))+"&tranid="+str(self.get_body_argument("trans"))+"&revamt="+str(self.get_body_argument("debit_amt"))
         req = requests.get(end_url, headers=headers, auth=('701e3938-c7c7-4568-9e3b-d474bfb39700', ''), verify=False)
         json_out = req.json()
         print("json")
